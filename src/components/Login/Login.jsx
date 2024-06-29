732063426f7a8c363601c66b9aed4f7e8ead4c76
Santu-Hansariafood <santu@hansariafood.com>
2024-06-29 16:37:06 +0530

    added the spning color

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -16,8 +16,8 @@ const LoginForm = ({ onLoginSuccess }) => {
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
         <div className="text-center">
-          <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-indigo-500 mx-auto mb-4"></div>
-          <h2 className="text-xl font-semibold">Login Please Wait...</h2>
+          <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-green-500 mx-auto mb-4"></div>
+          <h2 className="text-xl font-semibold text-yellow-500">Login Please Wait...</h2>
         </div>
       </div>
     );
--- src/components/common/Loading/Loading.jsx
+++ src/components/common/Loading/Loading.jsx
@@ -4,8 +4,8 @@ const Loading = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="text-center">
-        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500 mx-auto mb-4"></div>
-        <h2 className="text-xl font-semibold">Loading...</h2>
+        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-green-500 mx-auto mb-4"></div>
+        <h2 className="text-xl font-semibold text-yellow-500">Loading Please Wait...</h2>
       </div>
     </div>
   );
